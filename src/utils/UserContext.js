799dc9e9fd23e1e48fbaa5107498a5d0aae4d4ff
YallaJahnavi <yallajahnavi170@gmail.com>
2025-06-18 19:19:58 +0530

    Update UserContext.js

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,7 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
+// Create the context
 const UserContext = createContext({
-  loggedInUser: "Default User",
+  loggedInUser: "",
+  setUserName: () => {},
+  isLoggedIn: true,
+  setIsLoggedIn: () => {},
 });
+
+// Create a provider component
+export const UserContextProvider = ({ children }) => {
+  const [loggedInUser, setUserName] = useState("Guest");
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <UserContext.Provider
+      value={{
+        loggedInUser,
+        setUserName,
+        isLoggedIn,
+        setIsLoggedIn,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
