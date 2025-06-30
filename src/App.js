88e084ebad6fa6381fd2e418ec2c5a741a843d7f
YallaJahnavi <yallajahnavi170@gmail.com>
2025-06-30 14:08:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Contact from "./components/Contact";
 import Checkout from "./components/Checkout";
-import Order from "./components/Order";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
@@ -76,11 +75,6 @@ const appRouter = createBrowserRouter([
       { path: "/home/restaurants/:resId", element: <RestaurantMenu /> },
       { path: "/home/cart", element: <Cart /> },
       { path: "/home/checkout", element: <Checkout />},
-      {
-        path: "checkout/order", // ✅ Your desired route
-        element: <Order />,
-      },
-
 
     ],
   },
