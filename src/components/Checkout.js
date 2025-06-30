05d256457c14059c35d63e3088eb53bb9b7b29b8
YallaJahnavi <yallajahnavi170@gmail.com>
2025-06-30 14:08:15 +0530

    Update Checkout.js

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -10,14 +10,6 @@ const Checkout = () => {
     return total + price / 100;
   }, 0);
 
-  const handleOrder = () => {
-    alert("✅ Order placed successfully!");
-    // Redirect to Order.js page after alert is closed
-    setTimeout(() => {
-      navigate("/home/checkout/order");
-    }, 0);
-  };
-
   return (
     <div className="p-6 max-w-3xl mx-auto">
       <button
@@ -55,10 +47,10 @@ const Checkout = () => {
 
           <div className="text-center">
             <button
-              onClick={handleOrder}
+              onClick={() => alert("✅ Order placed successfully!")}
               className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700"
             >
-              Order
+              Place Order
             </button>
           </div>
         </>
