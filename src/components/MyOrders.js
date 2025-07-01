3c45062bc334d6fc5f175d26665d749f3ead2097
YallaJahnavi <yallajahnavi170@gmail.com>
2025-07-01 08:25:34 +0530

    Add files via upload

---
+++ src/components/MyOrders.js
@@ -0,0 +1,10 @@
+const MyOrders = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">My Orders</h1>
+      <p>Your order history will appear here.</p>
+    </div>
+  );
+};
+
+export default MyOrders;
