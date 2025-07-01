2a908211be4e054887d4c1813208d5cb3b1bb126
YallaJahnavi <yallajahnavi170@gmail.com>
2025-07-01 08:46:12 +0530

    Update appStore.js

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
-
+import ordersReducer from "./ordersSlice";
 const appStore = configureStore({
   reducer: {
     cart: cartReducer,
+    orders: ordersReducer,
   },
 });
 
