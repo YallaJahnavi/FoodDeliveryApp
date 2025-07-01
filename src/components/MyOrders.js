8eee03a68b6f4888f04658486425356f3c0082d3
YallaJahnavi <yallajahnavi170@gmail.com>
2025-07-01 10:40:47 +0530

    Update MyOrders.js

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -1,17 +1,25 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"; // updated
 import { useNavigate } from "react-router-dom";
+import { cancelOrder } from "../utils/ordersSlice"; // added
 
 const MyOrders = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch(); // added
   const orderHistory = useSelector((store) => store.orders.orderHistory);
 
   const handleBackToHome = () => {
     navigate("/home");
   };
 
+  const handleCancel = (orderId) => { // added
+    const confirmCancel = window.confirm("Are you sure you want to cancel this order?");
+    if (confirmCancel) {
+      dispatch(cancelOrder(orderId));
+    }
+  };
+
   return (
     <div className="p-6">
-      {/* ✅ Back to Home Button */}
       <div className="mb-4">
         <button
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
@@ -46,6 +54,13 @@ const MyOrders = () => {
               <p className="text-right font-semibold text-green-700">
                 Total: ₹{order.total}
               </p>
+
+              <button
+                className="mt-2 px-4 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+                onClick={() => handleCancel(order.id)}
+              >
+                Cancel Order
+              </button>
             </div>
           ))}
         </div>
